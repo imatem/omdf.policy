e3a9972fc8136baf455f1666f7f43e5aea09f9ad
gil-cano <gil.cano@gmail.com>
2014-04-06 22:19:09 -0500

    Add test setup

---
+++ src/omdf/policy/tests/test_setup.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""Setup/installation tests for this package."""
+
+from omdf.policy.testing import IntegrationTestCase
+
+import unittest2 as unittest
+
+
+class TestsInstall(IntegrationTestCase):
+    """Test installation of omdf.policy into Plone."""
+
+    def setUp(self):
+        """Custom shared utility setup for tests."""
+        self.portal = self.layer['portal']
+
+    def test_portal_title(self):
+        self.assertEqual(
+            u"Olimpiada Matemática del Distrito Federal",
+            self.portal.getProperty('title')
+        )
+
+    def test_portal_description(self):
+        self.assertEqual(
+            u"Bienvenido a la Olimpiada Matématica",
+            self.portal.getProperty('description')
+        )
+
+
+def test_suite():
+    """This """
+    return unittest.defaultTestLoader.loadTestsFromName(__name__)
